6c44a315b97072a86dd0965e3a3d505c87ee5ba5
devluanpereira <luan23107@gmail.com>
2024-10-25 08:05:06 -0300

    Up changes...

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,18 @@
+import "../../../public/assets/css/login.css";
+
+
+function Login() {
+    return <div className="row">
+        <div className="col-sm-4 col-md-6">
+            Part 1
+        </div>
+        <div className="col-sm-4 col-md-6">
+            Part 2
+        </div>
+        <div className="col-sm-4 col-md-6">
+            Part 2
+        </div>
+    </div>
+}
+
+export default Login;
